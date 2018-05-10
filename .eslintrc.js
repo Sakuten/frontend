0db31fffcc14d36e1906710ccf0cebaa934da117
coord.e <me@coord-e.com>
2018-05-10 22:57:14 +0900

    Add jsx linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
-    "extends": "standard",
-    "env": {
-      "browser": true
+  'extends': 'standard',
+  'plugins': [
+    'import',
+    'react'
+  ],
+  'rules': {
+    'linebreak-style': 'off',
+    'no-unused-vars': [2, { 'varsIgnorePattern': 'h' }],
+    'react/jsx-uses-vars': 2
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     }
-};
+  },
+  'env': {
+    'browser': true
+  }
+}
