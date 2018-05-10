2511c25486383d59bef99e2da6434931d180f6fa
coord.e <me@coord-e.com>
2018-05-10 18:08:23 +0900

    Add dropdown list

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const state = {
     password: "",
     username: "",
     token: savedToken || ""
+  },
+  data: {
+    classroom_list: [],
+    lottery_list: []
+  },
+  submission: {
+    classroom: "",
+    lottery: ""
   }
 }
 
@@ -40,9 +48,54 @@ const actions = {
           console.error(resp)
         })
     },
-    logout: () => (state, actions) => actions.setToken(""),
+    logout: () => (state, actions) => actions.setToken("")
+  },
+  submission: {
+    setClassroom: id => ({classroom: id}),
+    setLottery: id => ({lottery: id}),
+    apply: () => (state, actions) => {
+      fetch_api(`api/lotteries/${state.lottery}`, {
+        headers: {
+          'Authorization': 'Bearer '+state.token
+        }
+      })
+        .then(response => {
+          actions.setLotteryList(response.data.lotteries)
+        })
+        .catch(console.error)
+    }
+  },
+  data: {
+    setLotteryList: list => ({ lottery_list: list }),
+    fetchLotteryList: () => (state, actions) => {
+      fetch_api('api/lotteries', {})
+        .then(response => {
+          actions.setLotteryList(response.data.lotteries)
+        })
+          .catch(console.error)
+    },
+    setClassroomList: list => ({ classroom_list: list }),
+    fetchClassroomList: () => (state, actions) => {
+      fetch_api('api/classrooms', {})
+        .then(response => {
+          actions.setClassroomList(response.data.classrooms)
+        })
+          .catch(console.error)
+    }
   }
 }
+
+const LotterySelect = ({classroom}) => (state, actions) => (
+  <select name="lotteries" oninput={e => actions.submission.setLottery(e.target.value)}>
+    {
+      state.data.lottery_list
+        .filter(c => c.classroom_id == classroom)
+        .map(c =>
+          <option value={c.id}>第{c.index+1}公演</option>
+        )
+    }
+  </select>
+)
 
 const loginView = (state, actions) => (
   <div>
@@ -64,7 +117,17 @@ const loginView = (state, actions) => (
 
 const loggedinView = (state, actions) => (
   <div>
-    <h1>Logged in as {state.credentials.username} {state.credentials.token}</h1>
+    <h1>You are {state.credentials.username}</h1>
+    <select name="classrooms"
+      oncreate={() => {actions.data.fetchClassroomList(); actions.data.fetchLotteryList();}}
+      oninput={e => actions.submission.setClassroom(e.target.value)}>
+      {
+        state.data.classroom_list.map(c =>
+          <option value={c.id}>{c.grade} {c.name}</option>
+        )
+      }
+    </select>
+    {state.submission.classroom ? <LotterySelect classroom={state.submission.classroom} /> : null}
     <button onclick={actions.credentials.logout}>Logout</button>
   </div>
 )
