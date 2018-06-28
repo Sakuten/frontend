99e947e7298d235afb68b28e919beb1a750dad00
coord.e <me@coord-e.com>
2018-06-28 19:43:33 +0900

    Add: Add login form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { inject, observer } from 'mobx-react';
 
+@inject('credential')
+@observer
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input type="text" name="username" value={this.props.credential.username} onChange={e => { this.props.credential.username = e.target.value }} />
+        <input type="text" name="password" value={this.props.credential.password} onChange={e => { this.props.credential.password = e.target.value }} />
+        <button onClick={() => this.props.credential.login()}>Login</button>
+        {this.props.credential.token}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'mobx-react';
 import registerServiceWorker from './registerServiceWorker';
+import {CredentialStore} from './CredentialStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const credentialStore = new CredentialStore()
+
+ReactDOM.render(
+  <Provider credential={credentialStore}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
