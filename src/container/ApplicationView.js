33ba4f8179d6d7b7ab3657d0de2a058819ec9a6d
coord.e <me@coord-e.com>
2018-06-29 18:29:25 +0900

    Change: Separate <select> tags

--- src/container/ApplicationView.js
+++ src/container/ApplicationView.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 
 import ApplicationList from '../component/ApplicationList'
+import ClassroomSelect from '../component/ClassroomSelect'
+import LotterySelect from '../component/LotterySelect'
 
 const ApplicationView = ({user, application, event}) => {
   const {
@@ -20,24 +22,11 @@ const ApplicationView = ({user, application, event}) => {
       <h1>Logged in as {user.get('username')}</h1>
       <button onClick={onLogout}>Logout</button>
       <h2>Apply</h2>
-    <select name="classrooms" value={application.classroom} onChange={e => onChangeClassroom(e.target.value)}>
-      {application.classroom_list.map(c =>
-      <option value={c.id}>{c.grade} {c.name}</option>
-    )
-    }
-  </select>
-<select name="lotteries" onChange={e => onChangeLottery(e.target.value)}>
-      {
-        application.lottery_list
-          .filter(c => c.classroom_id === Number(application.classroom))
-          .map(c =>
-            <option value={c.id}>第{c.index + 1}公演</option>
-          )
-      }
-    </select>
-    <button onClick={onApply}>Apply</button>
-    <h2>Your Applications</h2>
-    <ApplicationList list={user.get('applications')} onCancel={onCancel} />
+      <ClassroomSelect list={application.classroom_list} value={application.classroom} onChange={onChangeClassroom} />
+      <LotterySelect classroom={application.classroom} list={application.lottery_list} value={application.lottery} onChange={onChangeLottery} />
+      <button onClick={onApply}>Apply</button>
+      <h2>Your Applications</h2>
+      <ApplicationList list={user.get('applications')} onCancel={onCancel} />
     </div>
   )
 }
