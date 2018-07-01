4d32b1b0b2dc957630695847c53c385b4fb09a66
coord.e <me@coord-e.com>
2018-07-01 17:41:49 +0900

    Add: Add tests to check if Store/Event can be constructed correctly

---
+++ src/event/__tests__/index.spec.js
@@ -0,0 +1,11 @@
+import { Store } from '../../store'
+import { Event } from '../../event'
+
+describe('Event', () => {
+  it('is constructed', () => {
+    const event = new Event(new Store())
+    expect(event).toBeDefined()
+    expect(event.credential).toBeDefined()
+    expect(event.application).toBeDefined()
+  })
+})
