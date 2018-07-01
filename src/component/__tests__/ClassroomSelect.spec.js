7c9ca7e77d1c2bffb23a8c9b0d07fe326a91e761
coord.e <me@coord-e.com>
2018-07-01 18:18:48 +0900

    Add: Add tests for ClassroomSelect

--- src/component/__tests__/ClassroomSelect.spec.js
+++ src/component/__tests__/ClassroomSelect.spec.js
@@ -4,7 +4,20 @@ import ClassroomSelect from '../ClassroomSelect'
 
 const setup = propOverrides => {
   const props = Object.assign({
-    list: [],
+    list: [
+      {
+        'grade': 5,
+        'id': 1,
+        'index': 0,
+        'name': 'A'
+      },
+      {
+        'grade': 5,
+        'id': 2,
+        'index': 1,
+        'name': 'B'
+      }
+    ],
     value: 0,
     onChange: jest.fn()
   }, propOverrides)
@@ -19,9 +32,32 @@ const setup = propOverrides => {
 
 describe('components', () => {
   describe('ClassroomSelect', () => {
-    test('render', () => {
+    it('render', () => {
       const { wrapper } = setup()
       expect(wrapper).toMatchSnapshot()
+    })
+
+    it('renders two options', () => {
+      const { wrapper } = setup()
+      expect(wrapper.find('[data-test="classroom-option"]').length).toBe(2)
+    })
+
+    it('renders with grade and name', () => {
+      const { wrapper } = setup()
+      expect(wrapper.find('[data-test="classroom-option"]').at(0).text()).toBe('5 A')
+      expect(wrapper.find('[data-test="classroom-option"]').at(1).text()).toBe('5 B')
+    })
+
+    it('calls onChange with id when something is selected', () => {
+      const mock = jest.fn()
+      const { wrapper } = setup({onChange: mock})
+      wrapper.find('[data-test="classroom-select"]').simulate('change', {target: {value: 2}})
+      expect(mock).toBeCalledWith(2)
+    })
+
+    it('renders specified id', () => {
+      const { wrapper } = setup({value: 2})
+      expect(wrapper.find('[data-test="classroom-select"]').props().value).toBe(2)
     })
   })
 })
