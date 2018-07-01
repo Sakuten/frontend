3ca979027161b76fd7a82998649c4f6727f5c0ea
coord.e <me@coord-e.com>
2018-07-01 17:45:04 +0900

    Change: Categorize test cases

--- src/event/__tests__/application.spec.js
+++ src/event/__tests__/application.spec.js
@@ -1,37 +1,39 @@
 import { ApplicationObject } from '../application'
 import { Store } from '../../store'
 
-describe('ApplicationObject', () => {
-  let event
+describe('events', () => {
+  describe('ApplicationObject', () => {
+    let event
 
-  beforeEach(() => {
-    event = new ApplicationObject(new Store())
-  })
+    beforeEach(() => {
+      event = new ApplicationObject(new Store())
+    })
 
-  it('fetches lists in constructor', () => {
-    expect(event.store.application.classroomList.length).not.toBe(0)
-    expect(event.store.application.lotteryList.length).not.toBe(0)
-  })
+    it('fetches lists in constructor', () => {
+      expect(event.store.application.classroomList.length).not.toBe(0)
+      expect(event.store.application.lotteryList.length).not.toBe(0)
+    })
 
-  it('changes classroom', () => {
-    event.onChangeClassroom(2)
-    expect(event.store.application.classroom).toBe(2)
-  })
+    it('changes classroom', () => {
+      event.onChangeClassroom(2)
+      expect(event.store.application.classroom).toBe(2)
+    })
 
-  it('changes lottery', () => {
-    event.onChangeLottery(2)
-    expect(event.store.application.lottery).toBe(2)
-  })
+    it('changes lottery', () => {
+      event.onChangeLottery(2)
+      expect(event.store.application.lottery).toBe(2)
+    })
 
-  it('applies to lottery', async () => {
-    await event.onApply()
-    const applications = event.store.credential.status.get('applications')
-    expect(applications).toBeDefined()
-  })
+    it('applies to lottery', async () => {
+      await event.onApply()
+      const applications = event.store.credential.status.get('applications')
+      expect(applications).toBeDefined()
+    })
 
-  it('cancels lottery', async () => {
-    await event.onCancel()
-    const applications = event.store.credential.status.get('applications')
-    expect(applications).toBeDefined()
+    it('cancels lottery', async () => {
+      await event.onCancel()
+      const applications = event.store.credential.status.get('applications')
+      expect(applications).toBeDefined()
+    })
   })
 })
--- src/event/__tests__/credential.spec.js
+++ src/event/__tests__/credential.spec.js
@@ -1,40 +1,42 @@
 import { CredentialObject } from '../credential'
 import { Store } from '../../store'
 
-describe('CredentialObject', () => {
-  let event
+describe('events', () => {
+  describe('CredentialObject', () => {
+    let event
 
-  beforeEach(() => {
-    event = new CredentialObject(new Store())
-  })
+    beforeEach(() => {
+      event = new CredentialObject(new Store())
+    })
 
-  it('changes username', () => {
-    event.onChangeUsername('username')
-    expect(event.store.credential.username).toBe('username')
-  })
+    it('changes username', () => {
+      event.onChangeUsername('username')
+      expect(event.store.credential.username).toBe('username')
+    })
 
-  it('changes password', () => {
-    event.onChangePassword('password')
-    expect(event.store.credential.password).toBe('password')
-  })
+    it('changes password', () => {
+      event.onChangePassword('password')
+      expect(event.store.credential.password).toBe('password')
+    })
 
-  it('clears the token in logout', () => {
-    event.onLogout()
-    expect(event.store.credential.token.length).toBe(0)
-  })
+    it('clears the token in logout', () => {
+      event.onLogout()
+      expect(event.store.credential.token.length).toBe(0)
+    })
 
-  it('can login', async () => {
-    await event.onLogin()
-    expect(event.store.credential.token.length).not.toBe(0)
-  })
+    it('can login', async () => {
+      await event.onLogin()
+      expect(event.store.credential.token.length).not.toBe(0)
+    })
 
-  it('clears password in login', async () => {
-    await event.onLogin()
-    expect(event.store.credential.password.length).toBe(0)
-  })
+    it('clears password in login', async () => {
+      await event.onLogin()
+      expect(event.store.credential.password.length).toBe(0)
+    })
 
-  it('fetches status in login', async () => {
-    await event.onLogin()
-    expect(event.store.credential.status.size).not.toBe(0)
+    it('fetches status in login', async () => {
+      await event.onLogin()
+      expect(event.store.credential.status.size).not.toBe(0)
+    })
   })
 })
