8693e130534e0c7a1c39ea1824ddf655d03d2a29
coord.e <me@coord-e.com>
2018-07-01 18:38:29 +0900

    Add: Add tests for LotterySelect

--- src/component/__tests__/LotterySelect.spec.js
+++ src/component/__tests__/LotterySelect.spec.js
@@ -4,9 +4,38 @@ import LotterySelect from '../LotterySelect'
 
 const setup = propOverrides => {
   const props = Object.assign({
-    list: [],
-    value: 0,
-    classroom: 0,
+    list: [
+      {
+        'classroom_id': 1,
+        'done': false,
+        'id': 3,
+        'index': 2,
+        'name': '5A.2'
+      },
+      {
+        'classroom_id': 1,
+        'done': false,
+        'id': 4,
+        'index': 3,
+        'name': '5A.3'
+      },
+      {
+        'classroom_id': 2,
+        'done': false,
+        'id': 5,
+        'index': 0,
+        'name': '5B.0'
+      },
+      {
+        'classroom_id': 2,
+        'done': false,
+        'id': 6,
+        'index': 1,
+        'name': '5B.1'
+      }
+    ],
+    value: 3,
+    classroom: 1,
     onChange: jest.fn()
   }, propOverrides)
 
@@ -20,9 +49,41 @@ const setup = propOverrides => {
 
 describe('components', () => {
   describe('LotterySelect', () => {
-    test('render', () => {
+    it('render', () => {
       const { wrapper } = setup()
       expect(wrapper).toMatchSnapshot()
+    })
+
+    it('renders two options', () => {
+      const { wrapper } = setup()
+      expect(wrapper.find('[data-test="lottery-option"]').length).toBe(2)
+      expect(wrapper.find('[data-test="lottery-option"]').at(0).props().value).toBe(3)
+      expect(wrapper.find('[data-test="lottery-option"]').at(1).props().value).toBe(4)
+    })
+
+    it('renders with human-readable messages', () => {
+      const { wrapper } = setup()
+      expect(wrapper.find('[data-test="lottery-option"]').at(0).text()).toBe('第3公演')
+      expect(wrapper.find('[data-test="lottery-option"]').at(1).text()).toBe('第4公演')
+    })
+
+    it('renders only in same classroom', () => {
+      const { wrapper } = setup({classroom: 2})
+      expect(wrapper.find('[data-test="lottery-option"]').length).toBe(2)
+      expect(wrapper.find('[data-test="lottery-option"]').at(0).props().value).toBe(5)
+      expect(wrapper.find('[data-test="lottery-option"]').at(1).props().value).toBe(6)
+    })
+
+    it('calls onChange with id when something is selected', () => {
+      const mock = jest.fn()
+      const { wrapper } = setup({onChange: mock})
+      wrapper.find('[data-test="lottery-select"]').simulate('change', {target: {value: 4}})
+      expect(mock).toBeCalledWith(4)
+    })
+
+    it('renders specified id', () => {
+      const { wrapper } = setup({value: 4})
+      expect(wrapper.find('[data-test="lottery-select"]').props().value).toBe(4)
     })
   })
 })
