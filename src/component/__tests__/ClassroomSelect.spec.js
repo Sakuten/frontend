ad45b080052b474a3ea18d77083c75df9ff4e1ac
coord.e <me@coord-e.com>
2018-07-01 19:00:58 +0900

    Change: Refactor ClassroomSelect testing

--- src/component/__tests__/ClassroomSelect.spec.js
+++ src/component/__tests__/ClassroomSelect.spec.js
@@ -26,7 +26,9 @@ const setup = propOverrides => {
 
   return {
     props,
-    wrapper
+    wrapper,
+    select: wrapper.find('[data-test="classroom-select"]'),
+    option: wrapper.find('[data-test="classroom-option"]')
   }
 }
 
@@ -38,26 +40,26 @@ describe('components', () => {
     })
 
     it('renders two options', () => {
-      const { wrapper } = setup()
-      expect(wrapper.find('[data-test="classroom-option"]').length).toBe(2)
+      const { option } = setup()
+      expect(option.length).toBe(2)
     })
 
     it('renders with grade and name', () => {
-      const { wrapper } = setup()
-      expect(wrapper.find('[data-test="classroom-option"]').at(0).text()).toBe('5 A')
-      expect(wrapper.find('[data-test="classroom-option"]').at(1).text()).toBe('5 B')
+      const { option } = setup()
+      expect(option.at(0).text()).toBe('5 A')
+      expect(option.at(1).text()).toBe('5 B')
     })
 
     it('calls onChange with id when something is selected', () => {
       const mock = jest.fn()
-      const { wrapper } = setup({onChange: mock})
-      wrapper.find('[data-test="classroom-select"]').simulate('change', {target: {value: 2}})
+      const { select } = setup({onChange: mock})
+      select.simulate('change', {target: {value: 2}})
       expect(mock).toBeCalledWith(2)
     })
 
     it('renders specified id', () => {
-      const { wrapper } = setup({value: 2})
-      expect(wrapper.find('[data-test="classroom-select"]').props().value).toBe(2)
+      const { select } = setup({value: 2})
+      expect(select.props().value).toBe(2)
     })
   })
 })
