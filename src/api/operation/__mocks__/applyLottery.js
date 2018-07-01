f22ab6acfe93db4d92a1d5baea5b6c1d5375ee77
coord.e <me@coord-e.com>
2018-07-01 17:08:32 +0900

    Add: Add API mocks and Load them

---
+++ src/api/operation/__mocks__/applyLottery.js
@@ -0,0 +1,5 @@
+import { responsify } from '../../../util/responsify'
+
+export const applyLottery = (lotteryId, token) => new Promise((resolve, reject) => {
+  resolve(responsify({'id': 24}))
+})
---
+++ src/api/operation/__mocks__/authenicate.js
@@ -0,0 +1,8 @@
+import { responsify } from '../../../util/responsify'
+
+export const authenicate = (username, password) => new Promise((resolve, reject) => {
+  resolve(responsify({
+    'message': 'Login Successful',
+    'token': 'gAAAAABbOIWaeB0_KFZ9rBGhZS1kBlLrMmeHC4m7T3xt5oGhiaowrZw_J_n2xoP04aAfLHUN8SSJjW6NtTE6r0Jd6CbYAa_cAAfkwHsWU1K6ywCL3mtqrHPb5SQwp3WkapuuXTEPeBrWEbPazD8lD5fxSJw-glVD8A=='
+  }))
+})
