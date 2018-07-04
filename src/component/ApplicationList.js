5b1cc3dad67cc95914764df381143f15789eaf59
coord.e <me@coord-e.com>
2018-07-04 21:27:05 +0900

    Add: Add styles to ApplicationList, which looks like cards

--- src/component/ApplicationList.js
+++ src/component/ApplicationList.js
@@ -5,9 +5,31 @@ const ApplicationList = ({list, onCancel}) => (
   <div>
     {
       list ? list.map(c =>
-        <div>
-          {JSON.stringify(c)}
-          <button className='delete is-danger' onClick={() => onCancel(c.lottery_id)}>Cancel</button>
+        <div className='card'>
+          <header className='card-header'>
+            <p className='card-header-title'>
+              ID {c.id}
+              <span className='m-r-sm m-l-sm'>
+                {
+                  c.status === null ? <span className='tag is-dark'>Pending</span>
+                    : (c.status ? <span className='tag is-success'>Won</span> : <span className='tag is-warning'>Rejected</span>)
+                }
+              </span>
+            </p>
+            <a href='#' className='card-header-icon' aria-label='more options' onClick={() => onCancel(c.lottery_id)}>
+              <span className='icon has-text-danger'>
+                <i className='fas fa-trash' aria-hidden='true' />
+              </span>
+            </a>
+          </header>
+          <div className='card-content'>
+            <div className='content'>
+              {JSON.stringify(c)}
+            </div>
+            <footer className='card-footer'>
+              <a href='#' className='card-footer-item  has-text-danger' onClick={() => onCancel(c.lottery_id)}>Cancel</a>
+            </footer>
+          </div>
         </div>
       ) : null
     }
