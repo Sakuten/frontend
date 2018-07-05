efe9491c7221514eedfc0df9e858946325781f53
coord.e <me@coord-e.com>
2018-07-05 15:53:36 +0900

    Change: Use `baseURL` of axios to join URL

--- src/util/api.js
+++ src/util/api.js
@@ -2,12 +2,11 @@ import axios from 'axios'
 import deepAssign from 'deep-assign'
 import docCookies from 'doc-cookies'
 
-export const apiServer = process.env.REACT_APP_API_SERVER
+export const apiServer = (process.env.NODE_ENV === 'development' && docCookies.getItem('API_SERVER')) || process.env.REACT_APP_API_SERVER
 export function fetchApi (endpoint, options) {
-  console.log(process.env.NODE_ENV)
-  const server = (process.env.NODE_ENV === 'development' && docCookies.getItem('API_SERVER')) || apiServer
   const opts = deepAssign(options, {
-    url: `${server}/${endpoint}`,
+    baseURL: apiServer,
+    url: endpoint,
     headers: {
       'Accept': 'application/json'
     },
