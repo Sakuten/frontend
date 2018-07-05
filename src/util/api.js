9ca86d686ab4148bf33466bfd3ee432498ea73a4
coord.e <me@coord-e.com>
2018-07-05 15:06:05 +0900

    Change: Use api server url from cookie if set

--- src/util/api.js
+++ src/util/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import deepAssign from 'deep-assign'
+import docCookies from 'doc-cookies'
 
 export const apiServer = process.env.REACT_APP_API_SERVER
 export function fetchApi (endpoint, options) {
+  const server = docCookies.getItem('API_SERVER') || apiServer
   const opts = deepAssign(options, {
-    url: `${apiServer}/${endpoint}`,
+    url: `${server}/${endpoint}`,
     headers: {
       'Accept': 'application/json'
     },
