baea589d4d3573fef165ea9525cef1031f2b3fe9
安藤春香 <haruan2394@gmail.com>
2018-07-12 23:42:26 +0900

    Add: Add router store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import {CredentialObject} from './credential'
 import {ApplicationObject} from './application'
+import { RouterStore } from 'mobx-react-router'
 import {ErrorObject} from './error'
 
 export class Store {
   constructor () {
     this.credential = new CredentialObject()
     this.application = new ApplicationObject()
+    this.router = new RouterStore()
     this.error = new ErrorObject()
   }
 }
