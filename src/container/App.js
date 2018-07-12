1394e2c1e9c7319d3d328eaf8bb7fe71d17b1859
安藤春香 <haruan2394@gmail.com>
2018-07-12 23:43:26 +0900

    Add: Add routing

--- src/container/App.js
+++ src/container/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import { Route, Redirect } from 'react-router'
 import LoginView from './LoginView'
 import ApplicationView from './ApplicationView'
 import ErrorList from '../component/ErrorList'
 
-const App = ({store, event}) => (
-  <div>
-    {store.credential.isLoggedIn ? <ApplicationView user={store.credential.status} application={store.application} /> : <LoginView credential={store.credential} />}
-    <ErrorList list={store.error.errorList} />
-  </div>
-)
+const App = ({store, event}) => {
+  const { location } = store.router
+  return (
+    <div location={location}>
+      <Route exact path='/' render={() => <p>Hello Here is Home  <a href='/lottery/login'>Login</a></p>} />
+      <Route path='/lottery/login' render={() => store.credential.isLoggedIn ? <Redirect to='/lottery' /> : <LoginView credential={store.credential} />} />
+      <Route exact path='/lottery' render={() => store.credential.isLoggedIn ? <ApplicationView user={store.credential.status} application={store.application} /> : <Redirect to='/lottery/login' />} />
+      <ErrorList list={store.error.errorList} />
+    </div>
+  )
+}
 
 export default inject('event')(observer(App))
