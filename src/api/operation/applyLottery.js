868e9e7c5fdba0ec5587536e9ec4e965ea1f1731
coord.e <me@coord-e.com>
2018-07-23 18:47:56 +0900

    Fix: Fix the endpoint of operation applyLottery

--- src/api/operation/applyLottery.js
+++ src/api/operation/applyLottery.js
@@ -1,6 +1,6 @@
 import {fetchApi} from '../request'
 
-export const applyLottery = (lotteryId, token) => fetchApi(`api/lotteries/${lotteryId}/apply`, {
+export const applyLottery = (lotteryId, token) => fetchApi(`api/lotteries/${lotteryId}`, {
   method: 'put',
   headers: {
     'Authorization': 'Bearer ' + token
