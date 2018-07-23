21d6e9fd5e384aa259018c1fc382731659d060fd
coord.e <me@coord-e.com>
2018-07-23 15:05:42 +0900

    Change: Separate <Home/> component

---
+++ src/component/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => (
+  <p>
+    Hello Here is Home
+    <a data-test='home-login' href='/lottery/login'>Login</a>
+  </p>
+)
+
+export default Home
--- src/container/App.js
+++ src/container/App.js
@@ -4,13 +4,14 @@ import { Route, Redirect } from 'react-router'
 import LoginView from './LoginView'
 import ApplicationView from './ApplicationView'
 import ErrorList from '../component/ErrorList'
+import Home from '../component/Home'
 
 const App = ({store, event}) => {
   const { location } = store.router
   return (
     <div location={location}>
       {store.credential.isLoggedIn}
-      <Route exact path='/' render={() => <p>Hello Here is Home  <a href='/lottery/login'>Login</a></p>} />
+      <Route exact path='/' component={Home} />
       <Route path='/lottery/login' render={() => store.credential.isLoggedIn ? <Redirect to='/lottery' /> : <LoginView credential={store.credential} />} />
       <Route exact path='/lottery' render={() => store.credential.isLoggedIn ? <ApplicationView user={store.credential.status} application={store.application} /> : <Redirect to='/lottery/login' />} />
       <ErrorList list={store.error.errorList} />
