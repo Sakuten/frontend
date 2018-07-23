e1fd14226e966b5fb101196286c4908475af4750
coord.e <me@coord-e.com>
2018-07-23 22:05:12 +0900

    Fix: Add a dependency to store.credential.isLoggedIn

--- src/container/App.js
+++ src/container/App.js
@@ -10,6 +10,7 @@ const App = ({store, event}) => {
   const { location } = store.router
   return (
     <div location={location}>
+      {store.credential.isLoggedIn}
       <Route exact path='/' component={Home} />
       <Route path='/lottery/login' render={() => store.credential.isLoggedIn ? <Redirect to='/lottery' /> : <LoginView credential={store.credential} />} />
       <Route exact path='/lottery' render={() => store.credential.isLoggedIn ? <ApplicationView user={store.credential.status} application={store.application} /> : <Redirect to='/lottery/login' />} />
