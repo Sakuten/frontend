6de4825548f2eeeb1b62e89aea89eddcf479fe8e
coord.e <me@coord-e.com>
2018-07-23 21:19:38 +0900

    Fix: Fix endpoint for cancel

--- src/api/operation/cancelLottery.js
+++ src/api/operation/cancelLottery.js
@@ -1,6 +1,6 @@
 import {fetchApi} from '../request'
 
-export const cancelLottery = (lotteryId, token) => fetchApi(`lotteries/${lotteryId}/apply`, {
+export const cancelLottery = (applicationId, token) => fetchApi(`applications/${applicationId}`, {
   method: 'delete',
   headers: {
     'Authorization': 'Bearer ' + token
