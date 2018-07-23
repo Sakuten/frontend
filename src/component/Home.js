74f5d86500c25f9539eccbb3b68393d10453be0c
coord.e <me@coord-e.com>
2018-07-23 15:30:06 +0900

    Change: Fix  the layout of Home

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Home = () => (
-  <p>
-    Hello Here is Home
+  <div>
+    <p>
+      Hello Here is Home
+    </p>
     <Link data-test='home-login' to='/lottery/login'>Login</Link>
-  </p>
+  </div>
 )
 
 export default Home
