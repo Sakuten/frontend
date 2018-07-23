a84cb6f17c9983b397bd5bed030e9addb8999fe7
coord.e <me@coord-e.com>
2018-07-23 20:43:23 +0900

    Add: Add a mock for /applications endpoint

---
+++ src/api/operation/__mocks__/getApplications.js
@@ -0,0 +1,18 @@
+import { responsify } from '../../../util/responsify'
+
+export const getClassrooms = () => new Promise((resolve, reject) => {
+  resolve(responsify([
+    {
+      "id": 1,
+      "lottery": {
+        "classroom_id": 1,
+        "done": false,
+        "id": 1,
+        "index": 0,
+        "name": "5A.0",
+        "winners": []
+      },
+      "status": "pending"
+    }
+  ]))
+})
