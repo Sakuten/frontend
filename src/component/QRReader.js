35d9ff05e8a5214c12ce34f211d3adc071e15a1a
coord.e <me@coord-e.com>
2018-07-26 13:19:01 +0900

    Add: Wait until reader is loaded

--- src/component/QRReader.js
+++ src/component/QRReader.js
@@ -1,19 +1,31 @@
 import React from 'react'
+import { observable, action } from 'mobx'
 import { observer } from 'mobx-react'
 
 import QrReader from 'react-qr-reader'
 
 @observer class QRReader extends React.Component {
+  @observable isLoading = true
+
   render () {
     return (
       <div>
-        <QrReader
-          onError={this.props.onError}
-          onScan={this.props.onScan}
-          style={{ width: '30%' }}
-        />
+        { this.isLoading && <p>Loading</p> }
+        <div style={{display: this.isLoading ? 'none' : 'block'}} >
+          <QrReader
+            onError={this.props.onError}
+            onScan={this.props.onScan}
+            onLoad={this.onLoad}
+            style={{ width: '30%' }}
+          />
+        </div>
       </div>
     )
+  }
+
+  @action.bound
+  onLoad () {
+    this.isLoading = false
   }
 }
 
