7430b7ad071c5caa7d7c5ff66317e6d141236705
coord.e <me@coord-e.com>
2018-08-08 00:47:28 +0900

    Add: Add tests of extractId

---
+++ src/util/__tests__/extractId.spec.js
@@ -0,0 +1,15 @@
+import {extractId} from '../extractId'
+
+describe('utils', () => {
+  describe('extractId', () => {
+    it('extracts secret id from url', () => {
+      const id = extractId('https://sakuten.jp/lottery/login?sid=secert_id')
+      expect(id).toBe('secert_id')
+    })
+
+    it('extracts null if url is invalid', () => {
+      const id = extractId('https://sakuten.jp/lotteries/login?sid=secert_id')
+      expect(id).toBeNull()
+    })
+  })
+})
