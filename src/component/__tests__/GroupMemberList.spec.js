b6736ae933c768b941c743bcd5c2622306b67dfd
coord.e <me@coord-e.com>
2018-08-08 00:57:48 +0900

    Add: Add tests of GroupMemberList

---
+++ src/component/__tests__/GroupMemberList.spec.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import GroupMemberList from '../GroupMemberList'
+
+const setup = propOverrides => {
+  const props = Object.assign({
+    list: [
+      'secret_id1',
+      'secret_id2',
+      'secret_id3',
+    ],
+    onRemove: jest.fn()
+  }, propOverrides)
+
+  const wrapper = shallow(<GroupMemberList {...props} />)
+
+  return {
+    props,
+    wrapper,
+    member: wrapper.find('[data-test="groupmemberlist-member"]'),
+    removeButton: wrapper.find('[data-test="groupmemberlist-remove"]')
+  }
+}
+
+describe('components', () => {
+  describe('GroupMemberList', () => {
+    it('render', () => {
+      const { wrapper } = setup()
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('renders nothing without list', () => {
+      const { member } = setup({list: null})
+      expect(member.length).toBe(0)
+    })
+
+    it('renders two members', () => {
+      const { member } = setup()
+      expect(member.length).toBe(3)
+    })
+
+    it('renders two cancel buttons', () => {
+      const { removeButton } = setup()
+      expect(removeButton.length).toBe(3)
+    })
+
+    it('calls onRemove with index when remove button is clicked', () => {
+      const mock = jest.fn()
+      const { removeButton } = setup({onRemove: mock})
+      removeButton.at(2).simulate('click')
+      expect(mock).toBeCalledWith(2)
+    })
+  })
+})
