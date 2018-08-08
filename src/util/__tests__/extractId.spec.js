a31b6280462097584fc3932d4d1058352a204380
coord.e <me@coord-e.com>
2018-08-08 18:24:02 +0900

    Fix: Fix typo

--- src/util/__tests__/extractId.spec.js
+++ src/util/__tests__/extractId.spec.js
@@ -3,17 +3,17 @@ import {extractId} from '../extractId'
 describe('utils', () => {
   describe('extractId', () => {
     it('extracts secret id from url', () => {
-      const id = extractId('https://sakuten.jp/lottery/login?sid=secert_id')
-      expect(id).toBe('secert_id')
+      const id = extractId('https://sakuten.jp/lottery/login?sid=secret_id')
+      expect(id).toBe('secret_id')
     })
 
     it('extracts from another domain', () => {
-      const id = extractId('https://sakuten-dev.netlify.com/lottery/login?sid=secert_id')
-      expect(id).toBe('secert_id')
+      const id = extractId('https://sakuten-dev.netlify.com/lottery/login?sid=secret_id')
+      expect(id).toBe('secret_id')
     })
 
     it('extracts null if url is invalid', () => {
-      const id = extractId('https://sakuten.jp/lotteries/login?sid=secert_id')
+      const id = extractId('https://sakuten.jp/lotteries/login?sid=secret_id')
       expect(id).toBeNull()
     })
   })
