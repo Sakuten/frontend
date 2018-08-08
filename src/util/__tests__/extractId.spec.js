cfeb93ea44bd1f72ad37e1ebce0b98220802e212
coord.e <me@coord-e.com>
2018-08-08 18:23:33 +0900

    Add: Add more test to extractId

--- src/util/__tests__/extractId.spec.js
+++ src/util/__tests__/extractId.spec.js
@@ -7,6 +7,11 @@ describe('utils', () => {
       expect(id).toBe('secert_id')
     })
 
+    it('extracts from another domain', () => {
+      const id = extractId('https://sakuten-dev.netlify.com/lottery/login?sid=secert_id')
+      expect(id).toBe('secert_id')
+    })
+
     it('extracts null if url is invalid', () => {
       const id = extractId('https://sakuten.jp/lotteries/login?sid=secert_id')
       expect(id).toBeNull()
