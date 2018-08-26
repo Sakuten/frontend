fe6b9e72f7e9b7e82a5691bf901f03f4d7819981
coord.e <me@coord-e.com>
2018-08-27 02:18:14 +0900

    Add: Add styles on applciation list items

--- src/component/ApplicationList.js
+++ src/component/ApplicationList.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import styled from 'styled-components'
+
+const Tag = styled.span.attrs({
+  className: 'tag'
+})`
+  margin: 0.75rem;
+`
 
 const ApplicationList = ({list, onCancel}) => (
   <div>
     {
       list ? list.map((c, i) =>
         <div data-test='applicationlist-application' key={i}>
-          {JSON.stringify(c)}
-          <button data-test='applicationlist-cancel' onClick={() => onCancel(c.id)}>Cancel</button>
+          <div className='card'>
+            <header className='card-header'>
+              <p className='card-header-title'>
+                {c.lottery.name}
+              </p>
+              {c.status === 'pending' ? <Tag className='is-dark'>発表をお待ちください</Tag> : c.status === 'won' ? <Tag className='is-success'>当選しました。</Tag> : <Tag className='is-danger'>落選しました。</Tag>}
+            </header>
+            <footer className='card-footer'>
+              <a className='card-footer-item has-text-danger' data-test='applicationlist-cancel' onClick={() => onCancel(c.id)}>キャンセル</a>
+            </footer>
+          </div>
         </div>
       ) : null
     }
