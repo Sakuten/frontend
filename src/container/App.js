06ded1d4d0a7533856924b1fb6df5e120495ef7c
coord.e <me@coord-e.com>
2018-08-26 21:20:41 +0900

    Add: Add an blurred image in background

--- src/container/App.js
+++ src/container/App.js
@@ -5,16 +5,22 @@ import LoginView from './LoginView'
 import ApplicationView from './ApplicationView'
 import ErrorList from '../component/ErrorList'
 import Home from '../component/Home'
+import styled from 'styled-components'
+import bg from '../sakuten.jpg'
+
+const Container = styled.div`
+  background-image: url(${bg});
+`
 
 const App = ({store, event}) => {
   const { location } = store.router
   return (
-    <div location={location}>
+    <Container location={location}>
       <Route exact path='/' component={Home} />
       <Route path='/lottery/login' render={() => store.credential.isLoggedIn ? <Redirect to='/lottery' /> : <LoginView credential={store.credential} />} />
       <Route exact path='/lottery' render={() => store.credential.isLoggedIn ? <ApplicationView user={store.credential.status} application={store.application} /> : <Redirect to='/lottery/login' />} />
       <ErrorList list={store.error.errorList} />
-    </div>
+    </Container>
   )
 }
 
