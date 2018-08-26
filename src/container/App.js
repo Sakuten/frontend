28ee32dd2f9445efc8a784e140648ca80c4e5b3e
coord.e <me@coord-e.com>
2018-08-27 01:39:46 +0900

    Fix: Stop using `position: fixed` on header and hide header in Home

--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { Route, Redirect } from 'react-router'
+import { withRouter } from 'react-router-dom'
 import LoginView from './LoginView'
 import ApplicationView from './ApplicationView'
 import ErrorList from '../component/ErrorList'
@@ -18,26 +19,28 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  position: fixed;
+  overflow-x: scroll;
 `
 
-const Heading = styled.header`
+const Heading = withRouter(styled.header`
   background-color: ${props => props.theme.header_color_upper};
-  position: fixed;
   width: 100%;
-  display: block;
+  display: ${props => props.location.pathname === '/' ? 'none' : 'block'};
   color: #000;
   z-index: 1;
   padding: 10px;
   font-size: 2rem;
   font-family: 'Roboto Condensed';
-`
+`)
 
 const App = ({store, event}) => {
   const { location } = store.router
   return (
-    <div>
+    <div location={location}>
       <Heading>KOISHIKAWA</Heading>
-      <Container location={location}>
+      <Container>
         <Route exact path='/' component={Home} />
         <Route path='/lottery/login' render={() => store.credential.isLoggedIn ? <Redirect to='/lottery' /> : <LoginView credential={store.credential} />} />
         <Route exact path='/lottery' render={() => store.credential.isLoggedIn ? <ApplicationView user={store.credential.status} application={store.application} /> : <Redirect to='/lottery/login' />} />
