ca62d6ef4cb42dc10173bc1b45716fd4e2839daf
coord.e <me@coord-e.com>
2018-08-26 18:41:01 +0900

    Add: Add a simple header in HOME, and write description in login button

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,10 +3,23 @@ import {Link} from 'react-router-dom'
 
 const Home = () => (
   <div>
-    <p>
-      Hello Here is Home
-    </p>
-    <Link data-test='home-login' to='/lottery/login'>Login</Link>
+    <nav className='level'>
+      <div className='level-left'>
+        <div className='level-item'>
+          KOISHIKAWA
+        </div>
+      </div>
+      <div className='level-right'>
+        <div className='level-item'>
+          <h1>行事週間</h1>
+          <h2>創作展</h2>
+        </div>
+      </div>
+    </nav>
+    <Link data-test='home-login' to='/lottery/login'>
+      <h3>QRコード読み取り</h3>
+      <p>(カメラが起動します)</p>
+    </Link>
   </div>
 )
 
