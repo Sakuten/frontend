291dc474fbf68841a607e2c223aa592fba6936d9
coord.e <me@coord-e.com>
2018-09-07 10:03:16 +0900

    Change: Just show matching lottery in lotteryselect

--- src/component/LotterySelect.js
+++ src/component/LotterySelect.js
@@ -2,16 +2,14 @@ import React from 'react'
 import { observer } from 'mobx-react'
 
 const LotterySelect = ({list, value, classroom, onChange}) => (
-  <div className='select'>
-    <select data-test='lottery-select' name='lotteries' value={value} onChange={e => onChange(e.target.value)}>
-      {
-        list
-          .filter(c => c.classroom_id === Number(classroom))
-          .map(c =>
-            <option data-test='lottery-option' key={c.id} value={c.id}>第{c.index + 1}公演</option>
-          )
-      }
-    </select>
+  <div>
+    {
+      list
+        .filter(c => c.classroom_id === Number(classroom))
+        .map(c =>
+          <span data-test='lottery-lottery' key={c.id} value={c.id}>第{c.index + 1}公演</span>
+        )
+    }
   </div>
 )
 
