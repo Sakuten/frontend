493a43084b8d773b04174630c94ac66781889032
coord.e <me@coord-e.com>
2018-09-07 10:05:44 +0900

    Change: Show a kind message when no lottery is returned

--- src/component/LotterySelect.js
+++ src/component/LotterySelect.js
@@ -4,11 +4,11 @@ import { observer } from 'mobx-react'
 const LotterySelect = ({list, value, classroom, onChange}) => (
   <div>
     {
-      list
+      list.length !== 0 ? list
         .filter(c => c.classroom_id === Number(classroom))
         .map(c =>
           <span data-test='lottery-lottery' key={c.id} value={c.id}>第{c.index + 1}公演</span>
-        )
+        ) : <span data-test='lottery-notfound'>現在応募は受け付けておりません。</span>
     }
   </div>
 )
