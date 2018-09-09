55cf592190d44401b4cc173e950225265255f08a
coord.e <me@coord-e.com>
2018-09-10 05:24:25 +0900

    Change: Detect ?staff and store it

--- src/store/credential.js
+++ src/store/credential.js
@@ -4,12 +4,14 @@ import qs from 'querystring'
 
 const savedToken = localStorage.getItem('Token')
 const paramSecretId = qs.parse(location.search.substr(1)).sid
+const isUsedByStaff = 'staff' in qs.parse(location.search.substr(1))
 
 export class CredentialObject {
   @observable secretId = paramSecretId || ''
   @observable recaptchaResponse = ''
   @observable token = savedToken || ''
   @observable status = new Map()
+  @observable isUsedByStaff = isUsedByStaff
 
   @computed get isLoggedIn () {
     return this.token.length !== 0
