0e77102e7582ee9588f325254ba8692fd5d7a919
coord.e <me@coord-e.com>
2018-09-10 07:34:59 +0900

    Change: Add some styles on group member item

--- src/component/GroupMemberList.js
+++ src/component/GroupMemberList.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import { observer } from 'mobx-react'
+
+const PublicIDWrap = styled.span`
+  width: 3rem;
+`
 
 const GroupMemberList = ({list, onRemove}) => (
   <div>
     {
       list.length !== 0 ? list.map((publicId, i) =>
         <div data-test='groupmemberlist-member' key={i}>
-          {publicId}
-          <button data-test='groupmemberlist-remove' onClick={() => onRemove(i)}>キャンセル</button>
+          <PublicIDWrap>
+            {publicId}
+          </PublicIDWrap>
+          <button className='button is-danger is-outlined' data-test='groupmemberlist-remove' onClick={() => onRemove(i)}>削除</button>
         </div>
       ) : <span data-test='groupmemberlist-notfound'>一緒に応募する人はいません。</span>
     }
