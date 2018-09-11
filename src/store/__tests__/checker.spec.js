22204a971c09b9414776f0aa0bb69a9f15a1ee89
coord.e <me@coord-e.com>
2018-09-12 03:06:58 +0900

    Add: Add a test of CheckerObject in store

---
+++ src/store/__tests__/checker.spec.js
@@ -0,0 +1,26 @@
+import { CheckerObject } from '../checker'
+
+describe('stores', () => {
+  describe('CheckerObject', () => {
+    let store
+
+    beforeEach(() => {
+      store = new CheckerObject()
+    })
+
+    it('can have classroom id', () => {
+      store.setClassroom(2)
+      expect(store.classroom).toBe(2)
+    })
+
+    it('can have last status', () => {
+      store.setLastStatus('Unknown')
+      expect(store.lastStatus).toBe('Unknown')
+    })
+
+    it('can have public id', () => {
+      store.setPublicId(2)
+      expect(store.publicId).toBe(2)
+    })
+})
+
