36ce139275dd4b838e4a63cebbf00cc7c0ce3f70
coord.e <me@coord-e.com>
2018-09-12 03:22:51 +0900

    Fix: Use the previous value 0.5rem

--- src/component/ApplicationList.js
+++ src/component/ApplicationList.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import StatusTag from './StatusTag'
 
 const TagWrapper = styled.span`
-  margin: 10px;
+  margin: 0.75rem;
 `
 
 const ApplicationList = ({list, onCancel}) => (
