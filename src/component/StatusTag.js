026d1241e70ea0637eef5471b88057e5c8889a74
coord.e <me@coord-e.com>
2018-09-11 18:08:03 +0900

    Change; Use two-part tag

--- src/component/StatusTag.js
+++ src/component/StatusTag.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import styled from 'styled-components'
 
-const Tag = styled.span.attrs({
-  className: 'tag'
-})`
-  margin: 0.75rem;
-`
-
-const StatusTag = ({status, className, children}) => {
+const StatusTag = ({status, className, left}) => {
   const tags = {
-    'pending': <Tag className={`is-dark ${className}`}>{children} 発表をお待ちください。</Tag>,
-    'won': <Tag className={`is-success ${className}`}>{children} 当選しました。</Tag>,
-    'lose': <Tag className={`is-danger ${className}`}>{children} 落選しました。</Tag>
+    'pending': ['is-dark', '発表をお待ちください。'],
+    'won': ['is-success', '当選しました。'],
+    'lose': ['is-danger', '落選しました。']
   }
 
-  return tags[status] || <Tag className={className}>{children} {status}</Tag>
+  const tagInfo = tags[status] || ['', status]
+  return (
+    <div className='tags has-addons'>
+      <span className='tag is-dark'>{left}</span>
+      <span className={`tag ${tagInfo[0]}`}>{tagInfo[1]}</span>
+    </div>
+  )
 }
 
 export default observer(StatusTag)
