815d77d5f5373a746f93d586c859ead7708a3048
coord.e <me@coord-e.com>
2018-09-14 14:52:56 +0900

    Add: Add a dialog component

---
+++ src/component/MessageDialog.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+
+const MessageDialog = ({title, content, buttonText, isOpen, onClose}) => (
+  <div className={`modal ${isOpen ? 'is-active' : ''}`}>
+    <div className='modal-background' />
+    <div className='modal-card'>
+      <header className='modal-card-head'>
+        <p className='modal-card-title'>{title}</p>
+        <button onClick={onClose} className='delete' aria-label='close' />
+      </header>
+      <section className='modal-card-body'>
+        {content}
+      </section>
+      <footer className='modal-card-foot'>
+        <button onClick={onClose} className='button is-success'>{buttonText}</button>
+      </footer>
+    </div>
+  </div>
+)
+
+export default observer(MessageDialog)
