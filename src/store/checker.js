539cd6e113144f22bc27ae228f77b2bdcc83f671
coord.e <me@coord-e.com>
2018-09-15 09:30:54 +0900

    Change: Store last classroom id on localstorage

--- src/store/checker.js
+++ src/store/checker.js
@@ -1,12 +1,15 @@
 import { observable, action } from 'mobx'
 
+const savedClassroom = localStorage.getItem('CheckerClassroom')
+
 export class CheckerObject {
-  @observable classroom = 1
+  @observable classroom = savedClassroom || 1
   @observable lastStatus = 'スキャンしてください'
   @observable publicId = ''
 
   @action.bound setClassroom (classroomId) {
     this.classroom = classroomId
+    localStorage.setItem('CheckerClassroom', classroomId)
   }
 
   @action.bound setLastStatus (status) {
