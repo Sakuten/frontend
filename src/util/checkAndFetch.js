0f711c12133ad6f50be92d8ca818207c9e8f8444
coord.e <me@coord-e.com>
2018-09-16 04:25:26 +0900

    Add: compare now with end_of_drawing and fatches if exceeded

--- src/util/checkAndFetch.js
+++ src/util/checkAndFetch.js
@@ -1,2 +1,11 @@
+import moment from 'moment'
+
 export const checkAndFetch = (store) => {
+  for (const {lottery: {end_of_drawing: time}} of store.application.applicationList) {
+    const diff = moment(time, 'HH:mm:ss')
+    if (diff < moment()) {
+      store.fetchStatus()
+      return
+    }
+  }
 }
