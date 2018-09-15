74d90692a8b27b0e8ece84e7ba648d201e948782
coord.e <me@coord-e.com>
2018-09-16 05:01:38 +0900

    Change: Show title in the option

--- src/component/ClassroomSelect.js
+++ src/component/ClassroomSelect.js
@@ -6,7 +6,7 @@ const ClassroomSelect = ({list, value, onChange}) => (
     <select data-test='classroom-select' name='classrooms' value={value} onChange={e => onChange(e.target.value)}>
       {
         list.map(c =>
-          <option data-test='classroom-option' key={c.id} value={c.id}>{c.grade} {c.name}</option>
+          <option data-test='classroom-option' key={c.id} value={c.id}>{c.grade}{c.name} {c.title}</option>
         )
       }
     </select>
