e8cf430dcc94b89511e8f3b607c31a705c3a4412
coord.e <me@coord-e.com>
2018-09-16 04:35:06 +0900

    Fix: Fix naming

--- src/util/checkAndFetch.js
+++ src/util/checkAndFetch.js
@@ -2,8 +2,8 @@ import moment from 'moment'
 
 export const checkAndFetch = (store) => {
   for (const {lottery: {end_of_drawing: time}} of store.application.applicationList) {
-    const diff = moment(time, 'HH:mm:ss')
-    if (diff < moment()) {
+    const endOfDrawing = moment(time, 'HH:mm:ss')
+    if (endOfDrawing < moment()) {
       store.fetchStatus()
       return
     }
