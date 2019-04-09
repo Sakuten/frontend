7823d7b3b3592720c82ee0d41457d9eb398054e5
Anharu <haruan2394@gmail.com>
2019-04-09 13:33:32 +0900

    Add: Build RooterMenu

--- src/component/FooterMenu.js
+++ src/component/FooterMenu.js
@@ -1,2 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
+
+const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  @media (min-width: 700px) {
+    display: none;
+  }
+  background: #87CEEB;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+`
+const Item = styled.div`
+  font-size: 30px;
+  color: #fff;
+`
+const FooterMenu = () => (
+  <Menu>
+    <Item>応募</Item>
+    <Item>マップ</Item>
+  </Menu>
+)
+
+export default FooterMenu
