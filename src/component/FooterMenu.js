4e383f21043de1b8c0f2fc50d441c9ae4817574a
Anharu <haruan2394@gmail.com>
2019-04-09 15:13:33 +0900

    Add: Create FooterMenu and move to page by pressing button

--- src/component/FooterMenu.js
+++ src/component/FooterMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import FontAwesome from 'react-fontawesome'
 
 const Menu = styled.div`
   display: flex;
@@ -12,17 +13,52 @@ const Menu = styled.div`
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: 50px;
+  height: 80px;
 `
 const Item = styled.div`
-  font-size: 30px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  width: 50%;
+`
+const Icon = styled.div`
   color: #fff;
 `
-const FooterMenu = () => (
-  <Menu>
-    <Item>応募</Item>
-    <Item>マップ</Item>
-  </Menu>
-)
+const Text = styled.p`
+  color: #fff;
+  font-size: 20px;
+`
 
-export default FooterMenu
+export default class FooterMenu extends React.Component {
+  moveToLottery = () => {
+    this.props.router.push('/')
+  }
+  moveToMap = () => {
+    this.props.router.push('/map')
+  }
+  render () {
+    return (
+      <Menu>
+        <Item onClick={this.moveToLottery}>
+          <Icon>
+            <FontAwesome name='poll' size='2x' />
+          </Icon>
+          <Text>
+            応募
+          </Text>
+        </Item>
+        <Item onClick={this.moveToMap}>
+          <Icon>
+            <FontAwesome name='map' size='2x' />
+          </Icon>
+          <Text>
+            マップ
+          </Text>
+        </Item>
+      </Menu>
+    )
+  }
+}
