47e5b6d7411b875d7055191934c523ee1fb66d06
Anharu <haruan2394@gmail.com>
2019-04-09 16:12:12 +0900

    Change: Change background by page

--- src/component/FooterMenu.js
+++ src/component/FooterMenu.js
@@ -24,6 +24,7 @@ const Item = styled.div`
   align-items: center;
   color: #fff;
   width: 50%;
+  background: ${props => props.isSelected ? '#78bfdc' : '#87CEEB'};
 `
 const Icon = styled.div`
   color: #fff;
@@ -36,14 +37,25 @@ const Text = styled.p`
 export default class FooterMenu extends React.Component {
   moveToLottery = () => {
     this.props.router.push('/lottery')
+    console.log(this.props.router)
   }
   moveToMap = () => {
     this.props.router.push('/map')
   }
+  isSelected = (type) => {
+    const path = this.props.router.location.pathname
+    let isSelected = false
+    if (type === 'lottery') {
+      isSelected = path === '/lottery' | path === '/' | path === '/lottery/login'
+    } else if (type === 'map') {
+      isSelected = path === '/map'
+    }
+    return isSelected
+  }
   render () {
     return (
       <Menu>
-        <Item onClick={this.moveToLottery}>
+        <Item onClick={this.moveToLottery} isSelected={this.isSelected('lottery')}>
           <Icon>
             <FontAwesome name='poll' size='3x' />
           </Icon>
@@ -51,7 +63,7 @@ export default class FooterMenu extends React.Component {
             応募
           </Text>
         </Item>
-        <Item onClick={this.moveToMap}>
+        <Item onClick={this.moveToMap} isSelected={this.isSelected('map')}>
           <Icon>
             <FontAwesome name='map' size='3x' />
           </Icon>
