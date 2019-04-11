211deec190a00fc861c9660ca4cbed95b9341323
Anharu <haruan2394@gmail.com>
2019-04-11 19:24:56 +0900

    Add: Add explanation of marks of school map

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import markImage from '../map_mark.png'
 import oneFloorImage from '../S6.1.png'
 import twoFloorImage from '../S8.2.png'
 import threeFloorImage from '../S10.3.png'
@@ -22,13 +23,24 @@ const FloorTitle = styled.h2`
   color: #000;
   margin: 20px 0;
 `
+
+const MarkTitle = styled.h4`
+  font-size: 1.2rem;
+  color: #000;
+  margin: 15px 0;
+`
 const MapImage = styled.img`
   width: 70vw;
   max-width: 500px;
 `
+const MarkImage = styled(MapImage)`
+  width: 30vw;
+`
 const Map = () => (
   <Container>
     <Title>校内地図</Title>
+    <MarkTitle>マークの説明</MarkTitle>
+    <MarkImage src={markImage} />
     <FloorTitle>一階</FloorTitle>
     <MapImage src={oneFloorImage} />
     <FloorTitle>二階</FloorTitle>
