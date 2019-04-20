4b51e26b118ed672050ae3047e1e7b13361a1768
Anharu <haruan2394@gmail.com>
2019-04-20 11:30:45 +0900

    Add: default select box is

--- src/component/ClassroomSelect.js
+++ src/component/ClassroomSelect.js
@@ -4,6 +4,7 @@ import { observer } from 'mobx-react'
 const ClassroomSelect = ({list, value, onChange}) => (
   <div className='select'>
     <select data-test='classroom-select' name='classrooms' value={value} onChange={e => onChange(e.target.value)}>
+      <option key='0' value='0'>クラスを選択してください</option>
       {
         list.map(c =>
           <option data-test='classroom-option' key={c.id} value={c.id}>{c.grade}{c.name} {c.title}</option>
