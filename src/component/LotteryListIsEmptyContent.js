5fff1378f7f81cf54e063cca47bb615bd117ea56
Anharu <haruan2394@gmail.com>
2019-09-06 00:37:57 +0900

    Change: Update dialog message

--- src/component/LotteryListIsEmptyContent.js
+++ src/component/LotteryListIsEmptyContent.js
@@ -4,7 +4,10 @@ const LotteryListIsEmptyContent = () => {
   return (
     <div>
       <p>
-        応募できる抽選がありません。応募受付時間でならば、下のボタンを押して再読み込みしてください
+        応募できる抽選がありません
+      </p>
+      <p>
+        応募受付時間になったら、下のボタンを押して再読み込みしてください
       </p>
       <button onClick={() => window.location.reload()}className='button is-info'>再読み読み</button>
     </div>
