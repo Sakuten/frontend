3edda1810f6ab281dba7faba5ee8401b0cdcde06
Anharu <haruan2394@gmail.com>
2019-09-09 00:09:46 +0900

    Add: Add PerformanceNumber

--- src/component/LotteryView.js
+++ src/component/LotteryView.js
@@ -6,12 +6,18 @@ const LotteryView = ({list, classroom, onChange}) => (
     {
       list.length !== 0 ? list
         .filter(c => c.classroom_id === Number(classroom))
-        .map(c => {
-          onChange(c.id)
-          return <span data-test='lottery-lottery' key={c.id} value={c.id}>第{c.index + 2}公演</span>
-        }) : <span data-test='lottery-notfound'>現在応募は受け付けておりません。</span>
+        .map(c => <PerformanceNumber onChange={onChange} c={c} />) : <span data-test='lottery-notfound'>現在応募は受け付けておりません。</span>
     }
   </div>
 )
 
+class PerformanceNumber extends React.Component {
+  componentWillMount () {
+    this.props.onChange(this.props.c.id)
+  }
+  render () {
+    const { c } = this.props
+    return <span data-test='lottery-lottery' key={c.id} value={c.id}>第{c.index + 2}公演</span>
+  }
+}
 export default observer(LotteryView)
